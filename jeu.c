78b35a5acd2575c442ad436dabf073d99427bee7
etbresso <etbresso@outlook.fr>
2015-04-06 15:56:43 +0200

    je sais plus ce que j'ai changé

--- jeu.c
+++ jeu.c
@@ -1,9 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <SDL2/SDL.h>
-#include <stdbool.h>
 #include "jeu.h"
 
 void jouer(){
-	 SDL_Log("Juste un test");
+	 SDL_Log("Juste un test qui tourne en boucle");
 }
