dbe2e6a0fb21b22405c030b85e2a681fb76275f1
etbresso <etbresso@outlook.fr>
2015-04-20 18:37:17 +0200

    début de l'ajout de la souris

--- jeu.c
+++ jeu.c
@@ -7,27 +7,34 @@ SDL_Surface* fond = NULL;
 SDL_Surface* bplante1 = NULL;
 int appel = 0;
 
-void interface(SDL_Surface* screen){ //crée l'interface du jeu
+void interfaceJeu(SDL_Surface* screen){ //crée l'interface du jeu
 
 	fond = SDL_LoadBMP( "images/fond.bmp" );
 	
 	bplante1 = SDL_LoadBMP("images/BPlante1.bmp");
-    SDL_Rect dest = { 144/2 - bplante1->w/2-8,128/2 - bplante1->h/2, 0, 0}; 
+    SDL_Rect boutonP1 = { 144/2 - bplante1->w/2-8,128/2 - bplante1->h/2, 0, 0}; 
 	
 	SDL_BlitSurface( fond, NULL, screen, NULL );
-    SDL_BlitSurface( bplante1, NULL, screen, &dest );
+    SDL_BlitSurface( bplante1, NULL, screen, &boutonP1 );
+
 }
 
 int appelJeu(){
 	return appel;
 }
 
-void quitter(){ //ferme les images appelées dans interface
+void quitterJeu(){ //ferme les images appelées dans interface
 	SDL_FreeSurface( bplante1 );
 	SDL_FreeSurface( fond );
 }
 
-void jouer(){
+void sourisJeu(int *x,int *y){
+	SDL_Log("Test clic gauche");
+	printf("%d \n", *x);
+	printf("%d \n", *y);
+}
+
+void principalJeu(){
 	appel = 1;
 }
 
