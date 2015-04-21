8780c83da1c856eb45b618272e51b3a8c3e7f827
etbresso <etbresso@outlook.fr>
2015-04-21 12:17:22 +0200

    ajout d'un prototype de fonction afin de corriger la fuite mémoire

--- jeu.h
+++ jeu.h
@@ -1,8 +1,9 @@
 #ifndef Jeu_H
 #define Jeu_H
 
-void interfaceJeu(SDL_Surface* screen);
+void interfaceJeu();
 int appelJeu();
+void actualisationJeu(SDL_Surface* screen);
 void quitterJeu();
 void sourisJeu(int *x,int *y);
 void principalJeu();
