a6845adda374d3aae031aba2fa3c9e7e7437008e
etbresso <etbresso@outlook.fr>
2015-04-22 12:08:26 +0200

    ajout d'une fonction afin de poser les plantes (sans ecrire 500 ligne de code)

--- jeu.h
+++ jeu.h
@@ -1,11 +1,12 @@
 #ifndef Jeu_H
 #define Jeu_H
 
+void principalJeu();
 void interfaceJeu();
 int appelJeu();
 void actualisationJeu(SDL_Surface* screen);
 void quitterJeu();
 void sourisJeu(int *x,int *y);
-void principalJeu();
+void posePlante(int i, int j);
 
 #endif
