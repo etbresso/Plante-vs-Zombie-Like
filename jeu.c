3e7ca6698ddb854cc037cf55cadbd1faec040eb9
etbresso <etbresso@outlook.fr>
2015-04-22 12:07:29 +0200

    il est possible de poser les 3 plante seulement sur la première ligne pour le moment

--- jeu.c
+++ jeu.c
@@ -3,15 +3,41 @@
 #include <SDL2/SDL.h>
 #include "jeu.h"
 
-int appel = 0;
+int appel = 0; // permet de savoir si on appel principalJeu();
 
 SDL_Surface* fond = NULL;
 SDL_Surface* bplante1 = NULL;
+SDL_Surface* bplante2 = NULL;
+SDL_Surface* bplante3 = NULL;
 
-int wBplante1 = NULL;
-int hBplante1 = NULL;
+SDL_Surface* plante[5][9]; //damier du jeu qui contindra les image des plantes
 
-int utilise = 0;
+//dim Bplante1
+int wBplante1 = NULL;//dimention de l'image en x
+int hBplante1 = NULL;//dimention de l'image en y
+//dim Bplante2
+int wBplante2 = NULL;//dimention de l'image en x
+int hBplante2 = NULL;//dimention de l'image en y
+//dim Bplante3
+int wBplante3 = NULL;//dimention de l'image en x
+int hBplante3 = NULL;//dimention de l'image en y
+
+int utilise = 0; //permet de savoir si on a cliqué sur le bouton d'une plante
+
+//------------------------------------------------------------------------------------
+void principalJeu(){
+	appel = 1;
+	int i;
+	int j;
+
+	//on initialise le tableau avec que des 0
+	for (i=0; i<5; i++){
+		for (j=0; j<5; j++){
+			plante[i][j]=NULL;
+		}
+	}
+
+}
 
 void interfaceJeu(){ //crée l'interface du jeu
 
@@ -19,8 +45,18 @@ void interfaceJeu(){ //crée l'interface du jeu
 	
 	//Bouton plante1
 	bplante1 = SDL_LoadBMP("images/BPlante1.bmp");
-	wBplante1 = 144/2 - bplante1->w/2-8;
-	hBplante1 = 128/2 - bplante1->h/2;
+	wBplante1 = 128/2 - bplante1->w/2;
+	hBplante1 = 144/2 - bplante1->h/2;
+
+	//Bouton plante2
+	bplante2 = SDL_LoadBMP("images/BPlante2.bmp");
+	wBplante2 = 128/2 - bplante2->w/2;
+	hBplante2 = 144/2 - bplante2->h/2 + 144;
+
+	//Bouton plante3
+	bplante3 = SDL_LoadBMP("images/BPlante3.bmp");
+	wBplante3 = 128/2 - bplante3->w/2;
+	hBplante3 = 144/2 - bplante3->h/2 + 288;
 
 }
 
@@ -28,9 +64,36 @@ void actualisationJeu(SDL_Surface* screen){//actualise les positions
 	//actualisation de la postion du fond
 	SDL_BlitSurface( fond, NULL, screen, NULL );
 
-	
+	//Postion Bouton
 	SDL_Rect dimBplante1 = { wBplante1,hBplante1, 0, 0}; //Position du bouton de la plante1
 	SDL_BlitSurface( bplante1, NULL, screen, &dimBplante1 );//actualisation de la postion du Bouton de la plante1
+
+	SDL_Rect dimBplante2 = { wBplante2,hBplante2, 0, 0}; //Position du bouton de la plante2
+	SDL_BlitSurface( bplante2, NULL, screen, &dimBplante2 );//actualisation de la postion du Bouton de la plante2
+
+	SDL_Rect dimBplante3 = { wBplante3,hBplante3, 0, 0}; //Position du bouton de la plante3
+	SDL_BlitSurface( bplante3, NULL, screen, &dimBplante3 );//actualisation de la postion du Bouton de la plante3
+
+	//Position Plante
+	// de plante[0][0] à plante[0][8]
+	SDL_Rect dimplante00 = { 128,0, 0, 0}; //Position  de la plante00
+	SDL_BlitSurface( plante[0][0], NULL, screen, &dimplante00 );//actualisation de la postion de la plante00
+	SDL_Rect dimplante01 = { 256,0, 0, 0}; 
+	SDL_BlitSurface( plante[0][1], NULL, screen, &dimplante01 );
+	SDL_Rect dimplante02 = { 384,0, 0, 0}; 
+	SDL_BlitSurface( plante[0][2], NULL, screen, &dimplante02 );
+	SDL_Rect dimplante03 = { 512,0, 0, 0}; 
+	SDL_BlitSurface( plante[0][3], NULL, screen, &dimplante03 );
+	SDL_Rect dimplante04 = { 640,0, 0, 0}; 
+	SDL_BlitSurface( plante[0][4], NULL, screen, &dimplante04 );
+	SDL_Rect dimplante05 = { 768,0, 0, 0}; 
+	SDL_BlitSurface( plante[0][5], NULL, screen, &dimplante05 );
+	SDL_Rect dimplante06 = { 896,0, 0, 0}; 
+	SDL_BlitSurface( plante[0][6], NULL, screen, &dimplante06 );
+	SDL_Rect dimplante07 = { 1024,0, 0, 0}; 
+	SDL_BlitSurface( plante[0][7], NULL, screen, &dimplante07 );
+	SDL_Rect dimplante08 = { 1152,0, 0, 0}; 
+	SDL_BlitSurface( plante[0][8], NULL, screen, &dimplante08 );
 }
 
 int appelJeu(){
@@ -44,9 +107,12 @@ void quitterJeu(){ //ferme les images appelées dans interface
 
 void sourisJeu(int *x,int *y){
 
+	//clic sur les bouton
 	if (*x>wBplante1 && *x<wBplante1+118 && *y>hBplante1 && *y<hBplante1+71 ){
-		if (utilise == 0){
+		if (utilise == 0 || utilise == 2 || utilise == 3){
 			bplante1 = SDL_LoadBMP("images/BPlante1Selc.bmp");
+			bplante2 = SDL_LoadBMP("images/BPlante2.bmp");
+			bplante3 = SDL_LoadBMP("images/BPlante3.bmp");
 			utilise = 1;
 		}
 		else{
@@ -54,13 +120,74 @@ void sourisJeu(int *x,int *y){
 			utilise = 0;
 		}
 	}
-	//printf("%d \n", *x);
-	//printf("%d \n", *y);
 
-	//printf("%d \n", wBplante1);
-	//printf("%d \n", hBplante1);
+	if (*x>wBplante2 && *x<wBplante2+118 && *y>hBplante2 && *y<hBplante2+71 ){
+		if (utilise == 0 || utilise == 1 || utilise == 3 ){
+			bplante1 = SDL_LoadBMP("images/BPlante1.bmp");
+			bplante2 = SDL_LoadBMP("images/BPlante2Selc.bmp");
+			bplante3 = SDL_LoadBMP("images/BPlante3.bmp");
+			utilise = 2;
+		}
+		else{
+			bplante2 = SDL_LoadBMP("images/BPlante2.bmp");
+			utilise = 0;
+		}
+	}
+
+	if (*x>wBplante3 && *x<wBplante3+118 && *y>hBplante3 && *y<hBplante3+71 ){
+			if (utilise == 0 || utilise == 1 || utilise == 2 ){
+				bplante1 = SDL_LoadBMP("images/BPlante1.bmp");
+				bplante2 = SDL_LoadBMP("images/BPlante2.bmp");
+				bplante3 = SDL_LoadBMP("images/BPlante3Selc.bmp");
+				utilise = 3;
+			}
+			else{
+				bplante3 = SDL_LoadBMP("images/BPlante3.bmp");
+				utilise = 0;
+			}
+		}
+
+
+	//clic sur une zone de jeu
+
+	// de plante[0][0] à plante[0][8]
+	if(*x>128 && *x<256 && *y>0 && *y<144 && plante[0][0] == NULL){
+		posePlante(0,0);
+	}
+	else if(*x>256 && *x<384 && *y>0 && *y<144 && plante[0][1] == NULL){
+		posePlante(0,1);
+	}
+	else if(*x>384 && *x<512 && *y>0 && *y<144 && plante[0][2] == NULL){
+		posePlante(0,2);
+	}
+	else if(*x>512 && *x<640 && *y>0 && *y<144 && plante[0][3] == NULL){
+		posePlante(0,3);
+	}
+	else if(*x>640 && *x<768 && *y>0 && *y<144 && plante[0][4] == NULL){
+		posePlante(0,4);
+	}
+	else if(*x>768 && *x<896 && *y>0 && *y<144 && plante[0][5] == NULL){
+		posePlante(0,5);
+	}
+	else if(*x>896 && *x<1024 && *y>0 && *y<144 && plante[0][6] == NULL){
+		posePlante(0,6);
+	}
+	else if(*x>1024 && *x<1152 && *y>0 && *y<144 && plante[0][7] == NULL){
+		posePlante(0,7);
+	}
+	else if(*x>1152 && *x<1280 && *y>0 && *y<144 && plante[0][8] == NULL){
+		posePlante(0,8);
+	}
 }
 
-void principalJeu(){
-	appel = 1;
+void posePlante(int i, int j){
+	if (utilise == 1){
+		plante[i][j]= SDL_LoadBMP("images/Plante1.bmp"); //on charge l'image a mettre
+	}
+	else if (utilise == 2){
+		plante[i][j] = SDL_LoadBMP("images/Plante2.bmp"); //on charge l'image a mettre
+	}
+	else if (utilise == 3){
+		plante[i][j] = SDL_LoadBMP("images/Plante3.bmp"); //on charge l'image a mettre
+	}
 }
