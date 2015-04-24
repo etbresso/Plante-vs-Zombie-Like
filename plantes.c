722d98c103ae02f670d1a950ea882e0d212fefca
etbresso <etbresso@outlook.fr>
2015-04-24 15:05:27 +0200

    le travail d'Halima pour le moment

---
+++ plantes.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "prototype.h"
+//créer une structure avant les méthodes;
+
+int main()
+{
+
+
+   return 0;
+
+}
+boolean etat_z(){
+if(zombie==1) return true;
+else return false;
+}
+// à définir dans zombie pour que je puisse l'utiliser
+
+
+void tuerZombie(){
+
+while(position_plants!=position_zombie && etat_z==true && position_zombie.y==position_plants.y){
+envoyer();// méthode qui envoie la balle
+}
+
+void envoyerBalle(){
+int sec=0
+
+}
+}
+
