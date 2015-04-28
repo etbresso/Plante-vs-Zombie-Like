b76baf4ee73f664aa5769f3b580e10642c0dc7ec
Halima <halimara@github.com>
2015-04-28 13:44:40 +0200

    plante structure

--- plantes.c
+++ plantes.c
@@ -1,31 +1,51 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "prototype.h"
-//créer une structure avant les méthodes;
+#include <time.h>
 
-int main()
-{
+typedef struct Plante Plante
+struct Plante(
+	int position_plante.x;
+	int position_plante.y;
+	int vie_plante;
+	String nom;
+);
 
-
-   return 0;
-
+Plante* Plante_construct(int x,int y, boolean vie_Plante, String nom){
+Plante *res=malloc(sizeof(Plante));
+res->x=x;
+res->y=y;
+res->vie_plante=vie_plante;
+res->nom=nom;
 }
+
+void Plante_destruct(Plante *p){
+free(p);
+}
+
 boolean etat_z(){
 if(zombie==1) return true;
 else return false;
 }
 // à définir dans zombie pour que je puisse l'utiliser
 
-
-void tuerZombie(){
-
-while(position_plants!=position_zombie && etat_z==true && position_zombie.y==position_plants.y){
-envoyer();// méthode qui envoie la balle
-}
-
 void envoyerBalle(){
-int sec=0
-
-}
+//int sec=0;
 }
 
+}
+void tuerZombie(){
+while(position_plants!=position_zombie && etat_z==1 && ){
+envoyerBalle();// méthode qui envoie la balle
+}
+
+void chargerBalle(){
+if(etatZombie==1 && position_zombie.y==position_plants.y){
+balle = SDL_LoadBMP("images/rouge.bmp");
+SDL_Rect  rouge = { 144/2 - balle->w/2-8,128/2 - balle->h/2, 0, 0};
+} 
+}
+void arreterBalle(){
+}
+
+}
