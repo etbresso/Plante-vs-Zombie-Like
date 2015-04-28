c302d0b1ab2d65e3290d3fb7154e6f9d8231e58e
Halima <halimara@github.com>
2015-04-28 13:45:37 +0200

    plante

--- plantes.h
+++ plantes.h
@@ -1,4 +1,10 @@
-#ifndef Plantes_H
-#define Plantes_H
+#ifndef plantes.c
+#define plantes.h
 
+typedef struct Plante Plante;
+Plante* Plante_construct(int x,int y, boolean vie_Plante, String nom);
+void Plante_destruct(Plante *p);
+boolean etat_z();
+void envoyerBalle();
+ 
 #endif
