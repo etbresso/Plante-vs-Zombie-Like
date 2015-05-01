b9396dc9a03d2bb9e0d3b3d16265a244419a1863
etbresso <etbresso@outlook.fr>
2015-05-01 09:53:39 +0200

    simplification du code

--- jeu.h
+++ jeu.h
@@ -6,7 +6,7 @@ void interfaceJeu();
 int appelJeu();
 void actualisationJeu(SDL_Surface* screen);
 void quitterJeu();
-void sourisJeu(int *x,int *y);
+void sourisJeu(int x,int y);
 void posePlante(int i, int j);
 
 #endif
