6f042d9daaf1f664945dcfe09098ac88f792e685
Halima <halimara@github.com>
2015-05-05 14:41:24 +0200

    ajout de fonctions et modofication de variables

--- plantes.c
+++ plantes.c
@@ -12,12 +12,25 @@ struct Plante{
 	char* type;
 };
 
-Plante* Plante_construct(int posx ,int posy,int vie_plante, char* nom){
+typedef struct Balle Balle;
+struct Balle{
+	int pos_bx;
+	int pos_by;
+};
+Balle* Balle_construct(int pos_bx ,int pos_by){
+Balle *res=malloc(sizeof(Balle));
+res->pos_bx=pos_bx;
+res->pos_by=pos_by;
+return res;
+}
+
+Plante* Plante_construct(int posx ,int posy,int vie_plante, char* nom, char* type){
 Plante *res=malloc(sizeof(Plante));
 res->posx=posx;
 res->posy=posy;
 res->vie_plante=vie_plante;
 res->nom=nom;
+res->type=type;
 return res;
 }
 /*
@@ -25,53 +38,31 @@ void Plante_destruct(Plante *p){
 free(p);
 }
 
-//int etat_z(){
-if(zombie==1) return true;
-else return false;
-}
-// à définir dans zombie pour que je puisse l'utiliser
-
-//void envoyerBalle(){
-//int sec=0;
+void envoyerBalle(){
+Balle *b;
+res=0;
+res->pos_bx;
 }
 
 }
 void tuerZombie(){
-while(position_plants!=position_zombie && etat_z==1 && ){
-envoyerBalle();// méthode qui envoie la balle
+while(x!=posx && vie_zombie>0 && posy==y){
+envoyerBalle();
 }
 
 void chargerBalle(){
-if(etatZombie==1 && position_zombie_y==posy){
+if(etatZombie==1 && y==posy){
 balle = SDL_LoadBMP("images/rouge.bmp");
 SDL_Rect  rouge = { 144/2 - balle->w/2-8,128/2 - balle->h/2, 0, 0};
 } 
-
-}
-
-//void arreterBalle(){
-}
-
-int getPosx(){
-return posx;
-}
-
-int getPosy(){
-return posy;
-}
-
-void setPosx(int dx,Plante p){
-int position_plante=dx;
-}
-
-void setPosy(int dy){
-int posy=dy;
 }
 
 void affichage(Plante *p){
- printf("%d(%d,%d,%d)",p->nom,p->position_plante.x,p->position_plante.y,p->vie_plante));
+ printf("%c(%d,%d,%d,%c)",p->type,p->posx,p->posy,p->vie_plante,p->nom);
 }
+void envoieSoleil(){
+SDL_Surface* fond = NULL;
+soleil = SDL_LoadBMP("images/soleil.bmp");
+SDL_Rect  rouge = { 144/2 - soleil->w/2-8,128/2 - soleil->h/2, 0, 0};
+} 
 
-}
-
-*/
--- plantes.h
+++ plantes.h
@@ -2,10 +2,14 @@
 #define plantes_H
 
 typedef struct Plante Plante;
-Plante* Plante_construct(int posx,int posy, int vie_plante, char* nom);
+Plante* Plante_construct(int posx,int posy, int vie_plante, char* nom, char* tye);
 //void Plante_destruct(Plante *p);
 //boolean etat_z();
 //void envoyerBalle();
 void affichage(Plante *p);
+int getPosx();
+typedef struct Balle Balle;
+Balle* Balle_construct(int pos_bx ,int pos_by);
+
  
 #endif
